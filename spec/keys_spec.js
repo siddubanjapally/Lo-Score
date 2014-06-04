4eb54eac61e1937b8956c2579807c8eed1b4a073
JayaKrishna <jayakrishna@krishna>
2014-06-04 17:46:35 +0530

    add specification file using karma and grunt

---
+++ spec/keys_spec.js
@@ -0,0 +1,11 @@
+describe(keys, function() {
+    it('takes an object, returns an array contains the keys of object', function() {
+        expect(keys({first:1, second:2, three:3, four:4})).toEqual(["first", "second", "three", "four"]);
+    });
+    it('if passed more than one object, returns first object keys', function() {
+        expect(keys({first: 1, second: 2}, {one:1, two:2})).toEqual(["first", "second"]);
+    });
+    it('if passed undefined, returns empty array', function() {
+        expect(keys()).toEqual([]);
+    });
+});
