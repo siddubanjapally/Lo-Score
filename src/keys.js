719385b81aa1628eaf70c38399e73f222b54b590
JayaKrishna <jayakrishna@krishna>
2014-06-04 17:44:46 +0530

    add keys.js file

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+var keys = function(obj) {
+    var result = [];
+    if(typeof obj !== 'object')
+        return [];
+    for(var key in obj){
+        if(obj.hasOwnProperty(key)){
+            result.push(key);
+        }
+    }
+    return result;
+};
