422da53ccf4857fad9f53beea1281b77376d6dd9
chinnaamara <amara.chinna@technoidentity.com>
2014-06-05 17:20:34 +0530

    add every

---
+++ src/every.js
@@ -0,0 +1,11 @@
+/**
+ * Created by chinna on 5/6/14.
+ */
+
+var every = function(arr, func) {
+    for(var i = 0; i < arr.length; ++i){
+        if(! func(arr[i]))
+            return false;
+    }
+    return true;
+};
