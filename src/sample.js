7799f0fcd7606a083d171a9956a0b94bacbc0450
chinnaamara <amara.chinna@technoidentity.com>
2014-06-06 15:46:59 +0530

    edit sample.js

--- src/sample.js
+++ src/sample.js
@@ -4,20 +4,18 @@
 
 var getResult = function(arr, num) {
     var result = [],
-        value,
-        length = arr.length,
-        index;
-    for(var i = 0; i < num && i < length; ++i){
+        arrayLength = arr.length,
+        value;
+    for(var i = 0; i < num && i < arrayLength; ++i){
         value = arr[Math.floor(Math.random() * arr.length)];
         result.push(value);
-        index = arr.indexOf(value);
-        arr.splice(index, 1);
+        arr.splice(arr.indexOf(value), 1);
     }
     return result;
 };
 
 var sample = function(arr, num) {
-    if(num || num == 0)
+    if(num || num === 0)
         return getResult(arr, num);
     else
         return arr[Math.floor(Math.random() * arr.length)];
