84c27dbea632e537515eda0498ae1c1d88c0acf0
chinnaamara <amara.chinna@technoidentity.com>
2014-06-10 17:19:19 +0530

    removed unnecessary additional functions from plucj.js and sample.js

--- src/pluck.js
+++ src/pluck.js
@@ -2,14 +2,9 @@
  * Created by chinna on 6/6/14.
  */
 
-var getValue = function(obj, propertyName) {
-    for(var key in obj)
-        return obj[propertyName];
-};
-
 var pluck = function(arr, propertyName) {
     var result = [];
     for(var i = 0; i < arr.length; ++i)
-        result.push(getValue(arr[i], propertyName));
+        result.push(arr[i][propertyName]);
     return result;
 };
--- src/sample.js
+++ src/sample.js
@@ -2,21 +2,38 @@
  * Created by chinna on 6/6/14.
  */
 
-var getResult = function(arr, num) {
+var sample = function(arr, num) {
     var result = [],
         arrayLength = arr.length,
         value;
-    for(var i = 0; i < num && i < arrayLength; ++i){
-        value = arr[Math.floor(Math.random() * arr.length)];
-        result.push(value);
-        arr.splice(arr.indexOf(value), 1);
+    if(num || num === 0){
+        for(var i = 0; i < num && i < arrayLength; ++i){
+            value = arr[Math.floor(Math.random() * arr.length)];
+            result.push(value);
+            arr.splice(arr.indexOf(value), 1);
+        }
+        return result;
     }
-    return result;
-};
-
-var sample = function(arr, num) {
-    if(num || num === 0)
-        return getResult(arr, num);
     else
         return arr[Math.floor(Math.random() * arr.length)];
 };
+
+
+//var getResult = function(arr, num) {
+//    var result = [],
+//        arrayLength = arr.length,
+//        value;
+//    for(var i = 0; i < num && i < arrayLength; ++i){
+//        value = arr[Math.floor(Math.random() * arr.length)];
+//        result.push(value);
+//        arr.splice(arr.indexOf(value), 1);
+//    }
+//    return result;
+//};
+
+//var sample = function(arr, num) {
+//    if(num || num === 0)
+//        return getResult(arr, num);
+//    else
+//        return arr[Math.floor(Math.random() * arr.length)];
+//};
