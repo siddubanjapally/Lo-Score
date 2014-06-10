495f35c7f30b3eba6bc74d5961d91a4b3ad2615e
ravi v <vravimca2011@gmail.com>
2014-06-10 16:51:59 +0530

    change unit test cases in compose_spec.js file

--- spec/compose_spec.js
+++ spec/compose_spec.js
@@ -1,8 +1,11 @@
 describe('compose',function() {
-    it('if we do not pass functions return error message',function() {
-        expect(compose()).toEqual('You have to pass functions');
-    });
-    it('compose the given two functions ',function() {
+    it('it takes two functions as parameters, first function takes a second function returned value and it returns final output ',function() {
         expect(compose(function(name) {return 'welcome ' + name;},function(stmt) {return stmt +' kumar';})('ravi')).toEqual('welcome ravi kumar');
+    });
+    it('if we give three functions as parameters, then we get final result ',function() {
+        var add = function(name) {return 10 + name;};
+        var multiply = function(num) { return num*2;};
+        var convertStringToNumber = function(str) { return +str;};
+        expect(compose(add,multiply,convertStringToNumber)('10')).toEqual(30);
     });
 });
--- src/compose.js
+++ src/compose.js
@@ -1,6 +1,5 @@
 var compose = function(){
     var funcs = arguments;
-    if(funcs.length === 0) return 'You have to pass functions';
     return function(){
         var args = arguments;
         for(var i=funcs.length-1;i>=0;i--){
