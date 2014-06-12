838812c13baa18d2f6eec87fb8d031adcdabd2a6
chinnaamara <amara.chinna@technoidentity.com>
2014-06-12 13:11:44 +0530

    add missed ';', use '===' instead of '==' and single quotes instead of double quotes for strings

--- spec/every_spec.js
+++ spec/every_spec.js
@@ -4,24 +4,24 @@
 
 describe('every', function() {
     it('takes an array and a predicate, each element from array passes into predicate, it returns true if predicate returns true for all elements else return false', function() {
-        expect(every([1, 2, 3, 4], function(num) { return num % 2 == 0 })).toEqual(false);
+        expect(every([1, 2, 3, 4], function(num) { return num % 2 === 0; })).toEqual(false);
     });
     it('takes an array and a predicate, each element from array passes into predicate, it returns true if predicate returns true for all elements else return false', function() {
-        expect(every([2, 4, 6, 8], function(num) { return num % 2 == 0 })).toEqual(true);
+        expect(every([2, 4, 6, 8], function(num) { return num % 2 === 0; })).toEqual(true);
     });
     it('takes an array and a predicate, each element from array passes into predicate, it returns true if predicate returns true for all elements else return false', function() {
-        expect(every([1, 2, 3, 4], function(num) { return num % 2 !== 0 })).toEqual(false);
+        expect(every([1, 2, 3, 4], function(num) { return num % 2 !== 0; })).toEqual(false);
     });
     it('takes an array and a predicate, each element from array passes into predicate, it returns true if predicate returns true for all elements else return false', function() {
-        expect(every([1, 3, 5, 7], function(num) { return num % 2 !== 0 })).toEqual(true);
+        expect(every([1, 3, 5, 7], function(num) { return num % 2 !== 0; })).toEqual(true);
     });
     it('takes an array and a predicate, each element from array passes into predicate, it returns true if predicate returns true for all elements else return false', function() {
-        expect(every([12, 25, 34], function(num) { return num % 5 == 0 })).toEqual(false);
+        expect(every([12, 25, 34], function(num) { return num % 5 === 0; })).toEqual(false);
     });
     it('takes an array and a predicate, each element from array passes into predicate, it returns true if predicate returns true for all elements else return false', function() {
-        expect(every([15, 20, 25, 30], function(num) { return num % 5 == 0 })).toEqual(true);
+        expect(every([15, 20, 25, 30], function(num) { return num % 5 === 0; })).toEqual(true);
     });
     it('if passed an empty array, returns true', function() {
-        expect(every([], function(num) { return num % 2 == 0 })).toEqual(true);
+        expect(every([], function(num) { return num % 2 === 0; })).toEqual(true);
     });
 });
