d80cfa7a71a444c2dfc4f418251c937adafd35b5
siddu <siddu.banjapally@gmail.com>
2014-06-13 09:51:02 +0530

    wrote sort method for sortby

--- src/sortby.js
+++ src/sortby.js
@@ -1,22 +1,27 @@
 /**
  * Created by sidhu on 9/6/14.
  */
-var mapArray = function(arr, f, init) {
-    for(var i = 0; i < arr.length; ++i)
-       init.push(f(arr[i]));
-    return init;
-};
 
-var sortBy = function(list,f) {
+var min = function(coll) {
+    var min = coll[0], index = -1;
+    for(var i = 0; i < coll.length; ++i) {
+        min = min < coll[i] ? min : coll[i];
+        index = min === coll[i] ? i : index;
+    }
+    return [min,index];
+
+}
+var sort = function(coll,init) {
+    var minIndex;
+    while(coll.length){
+        minIndex = min(coll);
+        init.push(minIndex[0]);
+        coll.splice(minIndex[1], 1);
+    }
+    return init;
+}
+var sortBy = function(list, f) {
     if(typeof f === 'function')
-        return mapArray(list,f,[]);
-    if(typeof f === 'undefined' || f === 'length')
-        return list.sort();
-    return list.sort(function (a, b) {
-        if (a[f] > b[f])
-            return 1;
-        if (a[f] < b[f])
-            return -1;
-        return 0;
-    });
+        return sort(map(list, f), []);
+     return sort(list, []);
 };
