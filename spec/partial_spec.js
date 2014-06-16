55c15394ab0ec17ce6672e0314905eb43f05cad3
ravi v <vravimca2011@gmail.com>
2014-06-16 18:44:10 +0530

    remove for loops

--- spec/partial_spec.js
+++ spec/partial_spec.js
@@ -1,11 +1,11 @@
 describe('partial',function() {
     it('it apply the given argument to the passing function', function() {
-        expect(partial(function(a){
+        expect(partial(function(a) {
             return a ;
-        },10)()).toEqual(10);
+        }, 10)()).toEqual(10);
     });
-    it('it apply the given partial two arguments to the passing function',function() {
-       var add10 = partial(function(a,b){return a + b ;},10);
+    it('it apply the given two arguments to the passing function',function() {
+       var add10 = partial(function(a, b){return a + b ;}, 10);
         expect(add10(10)).toEqual(20);
     });
     it('it apply the given partial three arguments to the passing function',function() {
--- src/partial.js
+++ src/partial.js
@@ -1,8 +1,7 @@
+var slice = Array.prototype.slice;
 var partial = function(func) {
-    var args = Array.prototype.slice.call(arguments,1);
+    var args = slice.call(arguments,1);
     return function() {
-        for(var i=0;i<arguments.length;i++)
-            args.push(arguments[i]);
-        return func.apply(this,args);
+        return func.apply(this, args.concat(slice.call(arguments)));
     };
 };
