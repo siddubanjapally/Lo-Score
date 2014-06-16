24d8832063267c0744d180c1c3f1611c5517149e
ravi v <vravimca2011@gmail.com>
2014-06-16 15:54:40 +0530

    remove for loops using reduce,map and isFunction

--- spec/uniq_spec.js
+++ spec/uniq_spec.js
@@ -8,7 +8,4 @@ describe('unique', function() {
     it('if we pass add iterate function then we have to unique that resulted array', function() {
         expect(uniq([1,3,4,1,5,4,3,6],function(value) {return value + 1;})).toEqual([2,4,5,6,7]);
     });
-    it('if we pass function and function context then we have to unique that resulted array', function() {
-        expect(uniq([1, 2.5, 3, 1.5, 2, 3.5], function(num) { return this.floor(num); }, Math)).toEqual([1,2,3]);
-    });
 });
--- src/uniq.js
+++ src/uniq.js
@@ -1,19 +1,9 @@
-var reduceArray = function(array) {
-    var res = [],i;
-    for(i in array){
-        if(res.indexOf(array[i]) === -1) res.push(array[i]);
-    }
-    return res;
-};
 
-var map = function(arr, f,context) {
-    var mapped = [];
-    for(var i = 0; i < arr.length; ++i)
-        mapped.push(f.call(context,arr[i]));
-    return mapped;
-};
-
-var uniq = function(array,iterator,context) {
-    if(arguments.length > 1) return reduceArray(map(array,iterator,context));
-    return reduceArray(array);
+var uniq = function(arr,iterator) {
+   if(!arr) return [];
+  return reduce(arguments.length > 1 ? map(arr,iterator) : arr, function(value,seed) {
+        if(seed.indexOf(value) === -1)
+            seed.push(value);
+       return seed;
+  },[]);
 };
