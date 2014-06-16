8730f70cc932e8dcf9626a8698db135a955c9913
ravi v <vravimca2011@gmail.com>
2014-06-16 18:50:07 +0530

    replace hasProperty  to has function

--- src/memoize.js
+++ src/memoize.js
@@ -1,11 +1,8 @@
-var hasProperty = function(obj,key) {
-    return obj.hasOwnProperty(key);
-};
 var memoize = function(func) {
     var memory = {};
     return function() {
         var arg = arguments[0];
-        if(hasProperty(memory,arg)) return memory[arg];
+        if(has(memory,arg)) return memory[arg];
         memory[arg] = func.call(null,arg);
         return memory[arg];
     };
