ee6f271c073fccda0f4c1885348e81cc273aa4b0
ravi v <vravimca2011@gmail.com>
2014-06-16 18:46:01 +0530

    add each.js and each_spec.js files

--- src/each.js
+++ src/each.js
@@ -1,15 +1,15 @@
 var each = function(obj,iterator,context) {
     if(obj === undefined) return obj;
-    context || null
+    context || null;
     if(obj.length === +obj.length){
         for(var i = 0; i < obj.length; i++) {
             iterator.call(context, obj[i], i, obj);
-        };
+        }
     } else {
         var keyslist = keys(obj);
-        for(var i = 0; i < keyslist.length; i++) {
-            iterator.call(context, obj[keyslist[i]], keyslist[i], obj);
-        };
+        for(var j = 0; j < keyslist.length; j++) {
+            iterator.call(context, obj[keyslist[j]], keyslist[j], obj);
+        }
     }
     return obj;
 };
